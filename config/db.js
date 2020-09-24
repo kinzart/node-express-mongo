cc07d80a6675cdd8b08327130d182fb3cdf8d8bc
Kinzart <68132346+kinzart@users.noreply.github.com>
2020-09-23 22:51:39 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,9 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: "mongodb+srv://dbsis:Q5jA2ivCptiezdsQ@learningnode.upjrg.mongodb.net/dbsis?retryWrites=true&w=majority" {useNewUrlParser: true}}
+    module.exports = {mongoURI: "mongodb://kinzart:paixonix2787@mongo_dbsisteminha:27017/dbsisteminha', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true}
    
 } else {
     module.exports = {mongoURI: "mongodb://localhost/blog"}
