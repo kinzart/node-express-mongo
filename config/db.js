29a60bb2ebda9638983678916ca73f2d734479c6
Kinzart <68132346+kinzart@users.noreply.github.com>
2020-09-25 23:19:55 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 if(process.env.NODE_ENV == "production"){
     module.exports = {mongoURI: "mongodb://kinzart:paixonix2787@mongo_dbsisteminha:27017/dbsisteminha"}
-   
+    console.log('mongodb online')
 } else {
     module.exports = {mongoURI: "mongodb://localhost/blog"}
+    console.log('mongodb local')
 }
